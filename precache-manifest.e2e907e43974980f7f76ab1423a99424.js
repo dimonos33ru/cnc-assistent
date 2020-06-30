149dbf391794a89195fee3d6f6da31c97bfd36ce
dimonos33ru <36456015+dimonos33ru@users.noreply.github.com>
2020-06-30 14:17:52 +0300

    Updates

---
+++ precache-manifest.e2e907e43974980f7f76ab1423a99424.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ba43f305b11e45043757364a4f2979",
+    "url": "/cnc-assistent/index.html"
+  },
+  {
+    "revision": "a998533d5192d4b73255",
+    "url": "/cnc-assistent/static/css/2.42ec2a3e.chunk.css"
+  },
+  {
+    "revision": "9146e0a75b2047860817",
+    "url": "/cnc-assistent/static/css/main.6362f2df.chunk.css"
+  },
+  {
+    "revision": "a998533d5192d4b73255",
+    "url": "/cnc-assistent/static/js/2.69eca234.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cnc-assistent/static/js/2.69eca234.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9146e0a75b2047860817",
+    "url": "/cnc-assistent/static/js/main.28e7e888.chunk.js"
+  },
+  {
+    "revision": "a88cd0fc6aab5479821c",
+    "url": "/cnc-assistent/static/js/runtime-main.01758b1d.js"
+  }
+]);
